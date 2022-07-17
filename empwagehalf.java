0a67bfb3a57b16ddbb3d1781c549a4f33ab50130
Mohit Raj <rajmohit.129@gmail.com>
2022-07-17 22:04:43 +0530

    [Mohit]Added:to check employee half

---
+++ empwagehalf.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+
+public class empwagehalf {
+
+    public static void main(String[] args) {
+        Random atte=new Random();
+        int max=3;
+        int half_time=4;
+        int wage_per_hour=20;
+        int hour_in_day=8;
+        int present=1;
+        int check=atte.nextInt(max);
+        int totalWage;
+        if(check == present)
+        {
+            totalWage=wage_per_hour*hour_in_day;
+            System.out.println("total Wage for Day : " + totalWage);
+        }
+        else if (check == 2)
+        {
+            totalWage=wage_per_hour*half_time;
+            System.out.println("PartTime Wage : " + totalWage);
+        }
+        else
+        {
+            System.out.println("Employee is absent");
+        }
+    }
+}

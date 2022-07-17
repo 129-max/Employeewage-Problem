f19ded8c57231d8ed238d5457324520333b4ff08
Mohit Raj <rajmohit.129@gmail.com>
2022-07-17 21:55:38 +0530

    []

---
+++ EmpwageC.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class EmpwageC {
+    public static void main(String[] args) {
+        Random att=new Random();
+        int max=2;
+        int wage_per_hour=20;
+        int hour_in_day=8;
+        int present=1;
+        int check=att.nextInt(max);
+        if(check == present)
+        {
+            int totalWage=wage_per_hour*hour_in_day;
+            System.out.println("total Wage for Day : " + totalWage);
+        }
+        else
+        {
+            System.out.println("Employee is absent");
+        }
+    }
+
+}
